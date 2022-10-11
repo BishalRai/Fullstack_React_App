a6bde0b35ef7a9bd2376ca6ae0bae64a572b59b0
Bishal Rai <bishal6193@gmail.com>
2022-10-12 03:42:34 +0545

    index js file has been modified.

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+const port = 3001
+
+// app.get("/",(req,res) => {
+//     res.status(200).json({message: "Node server is responding"})
+// })
+// app.listen(port,() => {
+//     console.log('Server running on port ${port}')
+// })
+
+app.listen(port)
