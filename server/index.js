af5785d1543683ff785449a533819566bfdaa2a1
Bishal Rai <bishal6193@gmail.com>
2022-10-12 04:09:11 +0545

    wrote get section on index js file

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ app.use(express.urlencoded({extended: false}))
 
 const port = 3001
 
-// app.get("/",(req,res) => {
-//     res.status(200).json({message: "Node server is responding"})
-// })
+app.get("/",(req,res) => {
+    res.status(200).json({message: "Node server is responding"})
+})
 // app.listen(port,() => {
 //     console.log('Server running on port ${port}')
 // })
