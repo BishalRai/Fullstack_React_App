467248e8883926a77cda82b06f4fa4f84d049121
Bishal Rai <bishal6193@gmail.com>
2022-10-17 01:22:08 +0545

    comments added to the db js file

--- server/services/db.js
+++ server/services/db.js
@@ -1,4 +1,6 @@
+//Database programming is done asynchronously so promises are used
 const mysql = require('mysql2/promise')
+//location of config file
 const config = require('../config')
 
 async function query(sql, params) {
