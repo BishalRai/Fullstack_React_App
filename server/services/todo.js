1ce25988cb67133a2bfe939f4cca8cc7095df180
Bishal Rai <bishal6193@gmail.com>
2022-10-17 03:02:50 +0545

    CREATE of CURD has been performed

--- server/services/todo.js
+++ server/services/todo.js
@@ -1,7 +1,17 @@
 const db = require('./db')
 const helper = require('../helper')
 
+
+//READ of CRUD operation
 async function getAllTasks() {
     const rows = await db.query('select * from task')
     return helper.emptyOrRows(rows)
+}
+
+
+//CREATE of CRUD operation
+async function addTask(task) {
+    const result = await db.query(`insert into task (description) values ('${task.description}')`) //here is back tick `` has use first after that '' quotation mark
+    task.id = result.insertId
+    return task
 }
