bff111a213e55ddb4f460501343db39308084961
Bishal Rai <bishal6193@gmail.com>
2022-10-17 04:38:08 +0545

    doen some necessary change to run the program

--- server/services/db.js
+++ server/services/db.js
@@ -6,6 +6,7 @@ const config = require('../config')
 async function query(sql, params) {
     const connection = await mysql.createConnection(config.db)
     const[result,] = await connection.execute(sql,params)
+    return result
 }
 
 module.exports = { query }
